8c6f02fc058c649d5d160a357a0cb4bcb2974cf3
Caleb <56181475+7248510@users.noreply.github.com>
2021-09-12 22:39:48 -0400

    Create sudoUser.c
    Small program/script to add a new user to the sudo group on Linux/Debian. You can run the command("sudo usermod -aG sudo username") but, I wanted to interact with the exec family.

---
+++ sudoUser.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main()
+{
+  puts("This script/program will add a new user to the sudo group\nTested on Debian");
+  const char *filePath = "/bin/sudo"; //Path of sudo
+  char *argv[] = {"/bin/sudo", "usermod", "-aG", "sudo", "test", NULL}; //arg vector| test is the name of the user. Swap the value
+  char *envp[] = {NULL};  
+  int rootAdd = execve(filePath, argv, envp);
+  if (!rootAdd) {
+    fprintf(stderr, "Error code: %s", strerror(errno)); 
+  }
+   
+}
